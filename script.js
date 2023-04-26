5734f55256f626142c99bbf49f683536e085fdd3
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-04-26 00:02:15 -0400

    fixed the button and added 4 sections

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Function to find information from the console.log and placed in the Html//
+
 function searchWeather(){
     var city = document.querySelector(".textVal").value
     var ApiKey = "7bdab0cf3daa341b1d431ecfe8584de8"
@@ -6,9 +8,14 @@ function searchWeather(){
     .then(data=>{
         console.log(data)
 
-        document.querySelector(".box-bodyToday").innerHTML="Wind Speed: "+data.wind.speed
+        document.querySelector(".box-bodyToday").innerHTML="Wind Speed: "+data.wind.speed+"MPH"
         document.querySelector(".box-bodyTemp").innerHTML="Temp: "+data.main.temp+"℉"
+        document.querySelector(".box-maxTemp").innerHTML="Max Temp: "+data.main.temp_max+"℉"
+        document.querySelector(".box-minTemp").innerHTML="Min Temp: "+data.main.temp_min+"℉"
+        document.querySelector(".box-Humidity").innerHTML="Humidity: "+data.main.humidity+"%"
+        document.querySelector(".boxCityName").innerHTML="City name: "+data.name
     })
 }
+
 
 document.querySelector("#button-addon2").addEventListener("click", searchWeather)
