9674d4b9e1d828e4a254f13d1b5a7c6a347228d3
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-04-26 14:54:53 -0400

    adding local storage

--- script.js
+++ script.js
@@ -14,11 +14,48 @@ function searchWeather(){
         document.querySelector(".box-minTemp").innerHTML="Min Temp: "+data.main.temp_min+"℉"
         document.querySelector(".box-Humidity").innerHTML="Humidity: "+data.main.humidity+"%"
         document.querySelector(".boxCityName").innerHTML="City name: "+data.name
+        document.querySelector(".icons").innerHTML="Icon: "+data.weather[0].icon
     })
 }
 
 
 document.querySelector("#button-addon2").addEventListener("click", searchWeather)
 
-var searchCities = document.querySelector("#cities-Searched");
-var dataStore= json.parse(localStorage.getItem("cities")) ||{};
+
+localStorage.setItem(".city", "myValue");
+var myValue = localStorage.getItem("city");
+
+//Array of Objects for localStores data
+var dataStore = JSON.parse(localStorage.getItem('texVal')) || [];
+
+var displayCities = function(){
+ 
+    cleaningElement(containerHistoricCities);
+
+    if(dataStore){
+        // creating a unordered list to store the info
+        var ulElement = document.createElement("ul");
+        ulElement.classList.add("list-unstyled");
+        ulElement.classList.add("w-100");
+        
+        //for loop to iterate through out the localStore
+        for(var i = 0; i < dataStore.length; i++){
+            
+            var liElement = document.createElement("li");
+            // append a button with bootstraps classes inside each item
+            // liElement.innerHTML = "<button type='button' class='list-group-item list-group-item-action' attr='"+dataStore[i]+"'>" + dataStore[i] + "</button>";
+            liElement.innerHTML = "<list-group row m-3' attr='"+dataStore[i]+"'>" + dataStore[i] + "</button>";
+            
+            // append the item into its container
+            ulElement.appendChild(liElement);
+            }
+
+            containerHistoricCities.appendChild(ulElement); 
+        }
+};
+
+
+
+
+// var searchCities = document.querySelector(".cities-Searched");
+// var dataStore= json.parse(localStorage.getItem("cities")) ||{};
