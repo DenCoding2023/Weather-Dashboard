fb5af98c89ee6e2ad11fd5118dddbda1e99bb1e5
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-04-26 23:39:22 -0400

    added a console log to the inner html and icon of weather.

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
 // Function to find information from the console.log and placed in the Html//
 
+
 function searchWeather(){
+
     var city = document.querySelector(".textVal").value
+
+    var searchHistory =[]
+    searchHistory.push(city);
+    console.log(searchHistory)
+    localStorage.setItem('search-history', JSON.stringify(searchHistory))
+    
+
+//    var city was here I moved it to the top to test///
     var ApiKey = "7bdab0cf3daa341b1d431ecfe8584de8"
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ApiKey+"&units=imperial")
     .then(res=>res.json())
     .then(data=>{
+        let weatherIcon= document.querySelector(".icons");
         console.log(data)
 
-        document.querySelector(".box-bodyToday").innerHTML="Wind Speed: "+data.wind.speed+"MPH"
-        document.querySelector(".box-bodyTemp").innerHTML="Temp: "+data.main.temp+"℉"
-        document.querySelector(".box-maxTemp").innerHTML="Max Temp: "+data.main.temp_max+"℉"
-        document.querySelector(".box-minTemp").innerHTML="Min Temp: "+data.main.temp_min+"℉"
-        document.querySelector(".box-Humidity").innerHTML="Humidity: "+data.main.humidity+"%"
-        document.querySelector(".boxCityName").innerHTML="City name: "+data.name
-        document.querySelector(".icons").innerHTML="Icon: "+data.weather[0].icon
+        document.querySelector(".box-bodyToday").innerHTML="Wind Speed: "+data.wind.speed+"MPH";
+        document.querySelector(".box-bodyTemp").innerHTML="Temp: "+data.main.temp+"℉";
+        document.querySelector(".box-maxTemp").innerHTML="Max Temp: "+data.main.temp_max+"℉";
+        document.querySelector(".box-minTemp").innerHTML="Min Temp: "+data.main.temp_min+"℉";
+        document.querySelector(".box-Humidity").innerHTML="Humidity: "+data.main.humidity+"%";
+        document.querySelector(".boxCityName").innerHTML="City name: "+data.name;
+        document.querySelector(".icons").innerHTML="Icon: "+data.weather[0].icon;
+        
+        let iconCode = data.weather[0].icon;
+  let iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+  weatherIcon.setAttribute("src", iconUrl);
+
     })
+    displayCities();
 }
+
 
 
 document.querySelector("#button-addon2").addEventListener("click", searchWeather)
 
 
-localStorage.setItem(".city", "myValue");
 var myValue = localStorage.getItem("city");
+localStorage.setItem(".city", myValue);
+
+
+function start(){
+
+loadCity();
+}
 
 //Array of Objects for localStores data
-var dataStore = JSON.parse(localStorage.getItem('texVal')) || [];
+var dataStore = JSON.parse(localStorage.getItem('search-history')) || [];
 
 var displayCities = function(){
+    let citySearchContainer= document.querySelector("#city-search");
  
-    cleaningElement(containerHistoricCities);
+    // cleaningElement(containerHistoricCities);
 
     if(dataStore){
         // creating a unordered list to store the info
@@ -43,8 +68,8 @@ var displayCities = function(){
             
             var liElement = document.createElement("li");
             // append a button with bootstraps classes inside each item
-            // liElement.innerHTML = "<button type='button' class='list-group-item list-group-item-action' attr='"+dataStore[i]+"'>" + dataStore[i] + "</button>";
-            liElement.innerHTML = "<list-group row m-3' attr='"+dataStore[i]+"'>" + dataStore[i] + "</button>";
+            liElement.innerHTML = "<button type='button' class='list-group-item list-group-item-action' attr='"+dataStore[i]+"'>" + dataStore[i] + "</button>";
+            // liElement.innerHTML = "<list-group row m-3' attr='"+dataStore[i]+"'>" + dataStore[i] + "</button>";
             
             // append the item into its container
             ulElement.appendChild(liElement);
