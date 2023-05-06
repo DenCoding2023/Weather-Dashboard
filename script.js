8f53986daa193bae0ce5eb303da263547e9b06a5
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-05-05 21:25:38 -0400

    finalizing the page

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
+var today = dayjs();
+var dayWeek = today.format('DD/MM/YY');
+$('#2a').text(dayWeek);
+
 
 const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+
 
 function searchWeatherAndLocalStorage() {
 
@@ -36,7 +42,7 @@ function searchWeatherAndLocalStorage() {
             document.querySelector(".box-minTemp").innerHTML = "Min Temp: " + data.main.temp_min + "℉";
             document.querySelector(".box-Humidity").innerHTML = "Humidity: " + data.main.humidity + "%";
             document.querySelector(".boxCityName").innerHTML = "City name: " + data.name;
-            document.querySelector(".icons").innerHTML = "Icon: " + data.weather[0].icon;
+            // document.querySelector(".icons").innerHTML = "Icon: " + data.weather[0].icon;
 
             let iconCode = data.weather[0].icon;
             let iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
@@ -54,7 +60,7 @@ document.querySelector("#button-addon2").addEventListener("click", searchWeather
 
 var myValue = localStorage.getItem("city");
 localStorage.setItem(".city", myValue);
-console.log(myValue)
+// console.log(myValue)
 
 
 function start() {
@@ -103,104 +109,12 @@ var displayCities = function (cities) {
 };
 
 
-// listener or call function when is clicked on button on each city history using Jquery
 
-
-// $(document).on("click", '.col-3 col-md-3 col-xl-3 colThrew', function(event) {
-
-//     event.preventDefault();
-
-//getting the attribute that contain the name of the city
-//     var city = $(this).attr("attr");
-//     callApiFetch(city);
-// });
-
-// Store the city in localStore
-// var saveCity = function (city) {
-
-//     var flag = false
-//     if (dataStore) {
-//         for (var i = 0; i < dataStore.length; i++) {
-//             if (dataStore[i] === city) {
-//                 flag = true;
-//             }
-//         }
-//         if (flag) {
-//             displayAlertMessage("The City: " + city + " already exists")
-//             //return
-//         }
-//     }
-//     if (!flag) {
-//         dataStore.push(city);
-//         localStorage.setItem("cities", JSON.stringify(dataStore));
-//     }
-
-//     loadCity();
-// }
-// var searchForDate9AM = function (str) {
-//     var hour = str.split(" ")[1].split(":")[0];
-//     var flag = false;
-
-//     if (hour === "09") {
-//         flag = true;
-//     }
-
-//     return flag;
+// var clearElement = function (element) {
+//     element.innerHTML = "";
 // };
 
 
-
-
-
-
-
-
-
-
-
-// Creating a variable to clear the innerhtml of the main area top right//
-
-var clearElement = function (element) {
-    element.innerHTML = "";
-};
-
-
-
-
-
-
-
-// testing to see if I can add a card//
-
-// const item = {
-//     name: 'Product 1',
-//     price: 10.99,
-//     quantity: 1
-
-// };
-
-
-// .fuction to dispaly history 
-// function displayHistory() {
-//     const searchHistoryBox = document.querySelector("searchHistory");
-//     searchHistoryBox.innerHTML = "";
-//     for (var i = 0; i < searchHistory.length; i++) {
-//       const savedCity = document.createElement("a");
-//             savedCity.classList =
-//         "list-item flex-row justify-space-between align-center saved-city";
-//       savedCity.setAttribute =
-//         ("href", "./index.html?city-name=" + searchHistory[i]);
-//       savedCity.textContent = searchHistory[i];
-
-
-//       (function (city) {
-//         savedCity.addEventListener("click", function () {
-//           cityLookup(city);
-//         });
-//       })(searchHistory[i]);
-//       searchHistoryBox.appendChild(savedCity);
-//     }
-//   }
 
 function searchWeather(item) {
 
@@ -263,9 +177,10 @@ function fiveDayforcast(lat, lon) {
                 var parentElement = document.createElement("div");
                 parentElement.setAttribute("class", "fc");
                 var divElement = document.createElement("div");
-                divElement.innerHTML = "Weather: " + element.weather[0].icon + "<br/>Date:" + element.dt_txt + "<br/>Temp:" + element.main.temp + "℉" + "<br/> Max Temp:" + element.main.temp_max +
-                    "<br/>Humidity:" + element.main.humidity + "%";
+                divElement.innerHTML = dayjs(element.dt_txt).format('MM/DD/YY')+ "<br/>Temp:" + element.main.temp + "℉" + "<br/> Max Temp:" + element.main.temp_max +
+                    "<br/>Humidity:" + element.main.humidity + "% <br/>Wind:" + element.wind.speed;
                
+                    // element.list[0].wind.speed +"MPH"
                 divElement.className = "fc";
 
                             
@@ -286,13 +201,7 @@ function fiveDayforcast(lat, lon) {
         
 }
 
-var formatDate = function(strDate){
-
-    var newDate = strDate.split(" ")[0].split("-");
-
-    return (newDate[1]+"/"+newDate[2]+"/"+newDate[0]);
-};
 
 
-displayCities();
+
 
